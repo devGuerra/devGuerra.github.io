1aee1e596f42da058c913739b32e7fdb7e7a2e82
Roberto Guerra <roberto.guerra@livup.com.br>
2020-12-15 22:41:59 -0300

    Updates

---
+++ precache-manifest.e78095edabfa480c0006180a46cde630.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b364a93dd8bea559d42cf0375ffe71ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbab538aaab728b3f448",
+    "url": "/static/css/main.3adc54ec.chunk.css"
+  },
+  {
+    "revision": "da339e97785ceb9aeee7",
+    "url": "/static/js/2.916a308a.chunk.js"
+  },
+  {
+    "revision": "402769e0fde7682650babf6b5720df16",
+    "url": "/static/js/2.916a308a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbab538aaab728b3f448",
+    "url": "/static/js/main.96be3250.chunk.js"
+  },
+  {
+    "revision": "7a6956e196ccd2e4ccc8856708b02847",
+    "url": "/static/js/main.96be3250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aa3d7f9ac5e47205f7b",
+    "url": "/static/js/runtime-main.d7651f20.js"
+  },
+  {
+    "revision": "2c803bb34afb999d6753286dd304faf5",
+    "url": "/static/media/brand-name.2c803bb3.png"
+  },
+  {
+    "revision": "276c1f07e371db8a8056b3088b8ab9e7",
+    "url": "/static/media/ciatoy.276c1f07.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4ffd4fe7945af123788bf5888296c696",
+    "url": "/static/media/github.4ffd4fe7.svg"
+  },
+  {
+    "revision": "87be59a1f7061fc6021876aad6fee028",
+    "url": "/static/media/google.87be59a1.svg"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/static/media/ill-2.594b1ee1.svg"
+  },
+  {
+    "revision": "1e7c70dc7d471d845aeed9e9bead0fe5",
+    "url": "/static/media/img-1-1200x1000.1e7c70dc.jpg"
+  },
+  {
+    "revision": "ac01559a7f34b02641cebd8c90d4544a",
+    "url": "/static/media/linkado.ac01559a.jpg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "682b11c944cd07ea2a1188109f9419cb",
+    "url": "/static/media/nutriblue.682b11c9.jpg"
+  },
+  {
+    "revision": "5a4f30d422992cd536e238fdc4509119",
+    "url": "/static/media/profile.5a4f30d4.png"
+  },
+  {
+    "revision": "75f7ab18f707114d817522df8dea0a3b",
+    "url": "/static/media/promo-1.75f7ab18.png"
+  },
+  {
+    "revision": "53033970a416368da35794389680266f",
+    "url": "/static/media/team-1-800x800.53033970.jpg"
+  },
+  {
+    "revision": "dcfcf3b77210fe85b0abc8260e6fa70e",
+    "url": "/static/media/team-2-800x800.dcfcf3b7.jpg"
+  },
+  {
+    "revision": "497bb3590e24c9f8b645864bfffc39b8",
+    "url": "/static/media/team-3-800x800.497bb359.jpg"
+  },
+  {
+    "revision": "230071328b705f8686cabd26a85ed6a5",
+    "url": "/static/media/team-4-800x800.23007132.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/devGuerra/devGuerra.github.io/precache-manifest.6496dc5d4a4670e246608526796adb46.js"
+  "/precache-manifest.e78095edabfa480c0006180a46cde630.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/devGuerra/devGuerra.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
